206b8e3a1006f07c9c013f1096dd0f5f06f71819
Mattia Righetti <matt95.righetti@gmail.com>
2022-11-12 23:13:59 +0100

    Initial Commit

---
+++ AdGuardHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdGuardHome
+//
+//  Created by Matt on 12/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
